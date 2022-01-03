e3f87ab5e099780b002134c2b392ad1e292a52c0
jaarvis23 <mundleyanushka@gmail.com>
2022-01-03 19:42:45 +0530

    deploy my react app to github pages

---
+++ src/components/Servicescards.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './Servicescards.css';
+
+function Servicescards() {
+  return (
+    <div class="crds">
+    <article class="crd">
+        <header>
+            <h2>Costa Rica</h2>
+            <br></br>
+        </header>    
+        <img src="images/1.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+          <br></br>
+            <p> Costa Rica is a rugged, rainforested Central American country with coastlines on the Caribbean and Pacific.</p>
+            <br></br>
+        </div>
+            
+    </article>
+            
+     <article class="crd">
+        <header>
+            <h2>Peru</h2>
+            <br></br>
+        </header>    
+        <img src="images/2.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>Peru is a country in South America that's home to a section of Amazon rainforest and Machu Picchu, 
+              high in the Andes mountains.</p>
+              <br></br>
+        </div>
+    </article>
+            
+    <article class="crd">
+        <header>
+            <h2>Switzerland</h2>
+            <br></br>
+        </header>
+        <img src="images/3.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>Switzerland is a mountainous Central European country, home to numerous lakes, 
+              villages and the high peaks of the Alps.</p>
+              <br></br>
+        </div>
+    </article>
+    <article class="crd">
+        <header>
+        
+        
+            <h2>India</h2>
+            <br></br>
+        </header>
+        <img src="images/4.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>India is a country in South Asia. It is the seventh-largest country by area, 
+               and the most populous democracy in the world.</p>
+        <br></br>
+        </div>
+
+    </article>
+    <br></br>
+    <article class="crd">
+        <header>
+        
+        
+            <h2>Netherlands</h2>
+            <br></br>
+        </header>
+        <img src="images/5.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>The Netherlands, a country in northwestern Europe, is known tulip fields, windmills and cycling routes.</p>
+        <br></br>
+        </div>
+
+    </article>
+    <article class="crd">
+        <header>
+        
+        
+            <h2>France</h2>
+            <br></br>
+        </header>
+        <img src="images/6.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>France encompasses medieval cities and Mediterranean beaches.
+            The country is also renowned for its wines and sophisticated cuisine.
+            </p>
+        <br></br>
+        </div>
+
+    </article>
+
+    <article class="crd">
+        <header>
+        
+        
+            <h2>Spain</h2>
+            <br></br>
+        </header>
+        <img src="images/7.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>Spain, a country on Europe’s Iberian Peninsula and  Capital city Madrid is home to the Royal Palace and Prado museum.
+            </p>
+        <br></br>
+        </div>
+
+    </article>
+
+    <article class="crd">
+        <header>
+        
+        
+            <h2>Turkey</h2>
+            <br></br>
+        </header>
+        <img src="images/8.jpeg" alt="Hot air balloons"/>
+        <div class="content">
+        <br></br>
+            <p>Destination Turkey, a Nations Online country profile of the Turkish nation located between Europe and Asia.
+            </p>
+        <br></br>
+        </div>
+
+    </article>
+
+</div>          
+    
+  );
+}
+
+export default Servicescards;
---
+++ src/components/pages/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+import Servicescards from '../Servicescards';
+
+function Services() {
+  return (
+    <>
+    <h1 className="services">SERVICES</h1>;
+      <Servicescards/>
+      <Footer />
+    </>
+  );
+}
+export default Services;
